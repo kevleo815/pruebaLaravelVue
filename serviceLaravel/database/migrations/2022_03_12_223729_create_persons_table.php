a980d397cac6052a22da174a9dd9f1ca1baad19c
kevleo815 <86327114+kevleo815@users.noreply.github.com>
2022-03-12 17:45:52 -0500

    Agregamos 3 campos en las migraciones
    Se ha agregado 3 campos en las migraciones de persons y jobs diferentes de los id y timestamp

--- serviceLaravel/database/migrations/2022_03_12_223729_create_persons_table.php
+++ serviceLaravel/database/migrations/2022_03_12_223729_create_persons_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("lastname");
+            $table->tinyInteger('age');
             $table->timestamps();
         });
     }
--- serviceLaravel/database/migrations/2022_03_12_223759_create_jobs_table.php
+++ serviceLaravel/database/migrations/2022_03_12_223759_create_jobs_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->string("role");
+            $table->string("nameEnterprise");
+            $table->string("city");
             $table->timestamps();
         });
     }
